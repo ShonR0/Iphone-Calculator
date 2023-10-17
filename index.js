7354fa87272184ac507eb9fec9dba41bde8fc1a3
Shon Rassan <112266979+ShonR0@users.noreply.github.com>
2023-10-18 01:26:50 +0300

    function that prints 1

---
+++ index.js
@@ -0,0 +1,7 @@
+    document.getElementById("printButton").addEventListener("click", function () {
+        // Get the input field
+        var outputField = document.getElementById("output");
+    
+        // Set the value of the input field to 1
+        outputField.value = "1";
+    });
