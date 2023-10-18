09486643730499f079f7ac7e01b1340129c85773
Shon Rassan <112266979+ShonR0@users.noreply.github.com>
2023-10-18 11:57:26 +0300

    added js behavior

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-    document.getElementById("printButton").addEventListener("click", function () {
-        // Get the input field
-        var outputField = document.getElementById("output");
-    
-        // Set the value of the input field to 1
-        outputField.value = "1";
-    });
+document.getElementById("clearButton").addEventListener("click", function () {
+    var outputField = document.getElementById("output");
+    outputField.value = ""; // Set the value to an empty string to clear the text.
+});
+document.getElementById("printOne").addEventListener("click", function () {
+    var outputField = document.getElementById("output");
+    outputField.value += "1";
+});
+document.getElementById("printTwo").addEventListener("click", function () {
+    var outputField = document.getElementById("output");
+    outputField.value += "2";
+});
+document.getElementById("printThree").addEventListener("click", function () {
+    var outputField = document.getElementById("output");
+    outputField.value += "3";
+});
+document.getElementById("printFour").addEventListener("click", function () {
+    var outputField = document.getElementById("output");
+    outputField.value += "4";
+});
+document.getElementById("printFive").addEventListener("click", function () {
+    var outputField = document.getElementById("output");
+    outputField.value += "5";
+});
+document.getElementById("printSix").addEventListener("click", function () {
+    var outputField = document.getElementById("output");
+    outputField.value += "6";
+});
+document.getElementById("printSeven").addEventListener("click", function () {
+    var outputField = document.getElementById("output");
+    outputField.value += "7";
+});
+document.getElementById("printEight").addEventListener("click", function () {
+    var outputField = document.getElementById("output");
+    outputField.value += "8";
+});
+document.getElementById("printNine").addEventListener("click", function () {
+    var outputField = document.getElementById("output");
+    outputField.value += "9";
+});
+document.getElementById("printTen").addEventListener("click", function () {
+    var outputField = document.getElementById("output");
+    outputField.value += "0";
+});
